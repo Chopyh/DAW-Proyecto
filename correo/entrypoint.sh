05f657e1c41b9829d5fd6ad531473420e79c4e3a
Chopyh <javierentrena2403@outlook.com>
2026-01-16 18:25:58 +0100

    Refactor Dockerfile, dovecot.conf y entrypoint.sh para mejorar la configuración de Postfix, manejo de logs y usuarios

--- correo/entrypoint.sh
+++ correo/entrypoint.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
-# --- correo/entrypoint.sh ---
 
-# 1. Configurar entorno de red para Postfix
+# 1. COPIA DE SEGURIDAD DE RED
+# Aunque desactivamos chroot, copiamos esto por si alguna librería lo busca
 mkdir -p /var/spool/postfix/etc
 cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
 cp /etc/hosts /var/spool/postfix/etc/hosts
 cp /etc/services /var/spool/postfix/etc/services
 chmod 644 /var/spool/postfix/etc/*
 
-# 2. Preparar el archivo de log (Truco para evitar errores)
-# Lo creamos vacío y le damos permisos totales para que rsyslog no falle al escribir
+# 2. PREPARAR LOGS (Vital para que no falle el tail)
 touch /var/log/mail.log
 chmod 666 /var/log/mail.log
 chown syslog:adm /var/log/mail.log
 
-# 3. Iniciar Syslog
+# 3. ARRANCAR SERVICIOS
 echo "Iniciando rsyslog..."
 service rsyslog start
 
-# 4. Iniciar Servicios de Correo
 echo "Iniciando Postfix..."
 service postfix start
 
 echo "Iniciando Dovecot..."
 service dovecot start
 
-# 5. Loop final mostrando el log
-echo "Servidor listo. Usuarios: javier / chopy. Logs:"
+# 4. BUCLE FINAL
+echo "Servidor de Correo (v.Final) Listo."
+echo "Usuarios: javier / chopy"
 tail -f /var/log/mail.log
